558aa0bd820e8c5104710b83143ab452b98d2bd8
Joaquimgmess <joaquimgmess21@gmail.com>
2026-08-15 14:02:37 -0300

    tui: cobrir a linha de lotacao no teste de tamanho

--- internal/tui/size_internal_test.go
+++ internal/tui/size_internal_test.go
@@ -22,6 +22,7 @@ func sizedSnapshot() client.Snapshot {
 			{
 				ID: 1, Kind: "viveiro_escavado", Fish: 2_000, MeanGrams: 306, FeedKg: 180,
 				OxygenUgL: 5_400, DensityMilli: 600, BatchID: 3, Capacity: 5_000, ServedFor: 240,
+				BreakEven: 967, StockAdvice: 1_240, CostPerFish: 599,
 				PriceKgCents: 678, ValueCents: 414_936, CostCents: 393_600, MarginCents: 21_336,
 				CostPerKg: 674, ClassPPM: 720_000, NextClassG: 600, NextClassGain: 220_000,
 				Decision: client.Decision{
@@ -35,6 +36,7 @@ func sizedSnapshot() client.Snapshot {
 			{
 				ID: 2, Kind: "tanque_rede", Fish: 1_400, MeanGrams: 612, FeedKg: 0,
 				OxygenUgL: 1_900, BatchID: 4, Capacity: 960, PriceKgCents: 829,
+				BreakEven: 1_820, StockAdvice: 0, CostPerFish: 599,
 				ValueCents: 754_120, MarginCents: 298_004, ClassPPM: 880_000, NextClassG: 900,
 				NextClassGain: 140_000,
 			},
